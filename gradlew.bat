45e99265ab13d5daf037e10da687cced20427cc7
veeramani2006 <cveeramani98@gmail.com>
2026-03-24 10:44:42 +0530

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@rem Gradle startup script for Windows
-@if "%DEBUG%" == "" @echo off
-set DIRNAME=%~dp0
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-set JAVACMD=java
-%JAVACMD% %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
